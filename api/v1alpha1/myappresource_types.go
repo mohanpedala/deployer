ae85605b5dc82c4c5aa02c23648113cf6d2bc0e5
Mohan Edala <mohan198631@gmail.com>
2024-04-01 22:24:47 -0500

    crd-0001 define crd and and generate api code

--- api/v1alpha1/myappresource_types.go
+++ api/v1alpha1/myappresource_types.go
@@ -1,41 +1,31 @@
-/*
-Copyright 2024.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package v1alpha1
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
-// MyAppResourceSpec defines the desired state of MyAppResource
 type MyAppResourceSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// Foo is an example field of MyAppResource. Edit myappresource_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	ReplicaCount int32 `json:"replicaCount"`
+	Resources    struct {
+		MemoryLimit string `json:"memoryLimit"`
+		CPURequest  string `json:"cpuRequest"`
+	} `json:"resources"`
+	Image struct {
+		Repository string `json:"repository"`
+		Tag        string `json:"tag"`
+	} `json:"image"`
+	UI struct {
+		Color   string `json:"color"`
+		Message string `json:"message"`
+	} `json:"ui"`
+	Redis struct {
+		Enabled bool `json:"enabled"`
+	} `json:"redis"`
 }
 
 // MyAppResourceStatus defines the observed state of MyAppResource
 type MyAppResourceStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	ObservedGeneration int64 `json:"observedGeneration,omitempty"`
 }
 
 //+kubebuilder:object:root=true
